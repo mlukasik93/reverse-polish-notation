d2d7262a599a82afa7ffd8605aff738f0010947b
mlukasik93 <mlukasik93@gmail.com>
2018-11-12 16:19:28 +0100

    2. Working, but without tests

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -14,7 +14,7 @@ public class Test implements ReversePolishNotation {
     @Override
     public long calculate(String expression) {
         long result;
-        long value ;
+        long value;
         List<String> tab = new ArrayList<>(Arrays.asList(expression.split(" ")));
 
         if (tab.isEmpty()) {
@@ -23,40 +23,30 @@ public class Test implements ReversePolishNotation {
             for (int i = 0; i < tab.size(); i++) {
                 if (tab.get(i).equals("+")) {
                     value = Long.parseLong(tab.get(i - 2)) + Long.parseLong(tab.get(i - 1));
-                    for(int j =0; j<3 ;j++){
-                        tab.remove(i-2);
-                    }
-                    tab.add(i-2,String.valueOf(value));
-                    i=0;
-
-
+                    i = listUpDate(value, tab, i);
                 } else if (tab.get(i).equals("-")) {
                     value = Long.parseLong(tab.get(i - 2)) - Long.parseLong(tab.get(i - 1));
-                    for(int j =0; j<3 ;j++){
-                        tab.remove(i-2);
-                    }
-                    tab.add(i-2,String.valueOf(value));
-                    i=0;
-
+                    i = listUpDate(value, tab, i);
                 } else if (tab.get(i).equals("*")) {
                     value = Long.parseLong(tab.get(i - 2)) * Long.parseLong(tab.get(i - 1));
-                    for(int j =0; j<3 ;j++){
-                        tab.remove(i-2);
-                    }
-                    tab.add(i-2,String.valueOf(value));
-                    i=0;
+                    i = listUpDate(value, tab, i);
                 } else if (tab.get(i).equals("/")) {
                     value = Long.parseLong(tab.get(i - 2)) / Long.parseLong(tab.get(i - 1));
-                    for(int j =0; j<3 ;j++){
-                        tab.remove(i-2);
-                    }
-                    tab.add(i-2,String.valueOf(value));
-                    i=0;
+                    i = listUpDate(value, tab, i);
                 }
 
             }
             result = Long.parseLong(tab.get(0));
         }
         return result;
+    }
+
+    private int listUpDate(long value, List<String> tab, int i) {
+        for (int j = 0; j < 3; j++) {
+            tab.remove(i - 2);
+        }
+        tab.add(i - 2, String.valueOf(value));
+        i = 0;
+        return i;
     }
 }
